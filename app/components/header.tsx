df5033ba9e52a5ca5d76e22b4961aab1cb0e6516
Indah Mutiah Utami MZ <indahmutiahmz@gmail.com>
2025-04-10 13:13:38 +0800

    feat: add components

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,3 +1,6 @@
+import { User } from "lucide-react";
+import { ShoppingCart } from "lucide-react";
+
 export function Header() {
   return (
     <header className="flex flex-row justify-between items-center w-full h-16 bg-card">
@@ -8,13 +11,14 @@ export function Header() {
           className="w-24 h-13 rounded-md ml-4"
         />
       </a>
-      <div className="flex flex-row items-center">
-        <button className="bg-primary text-white rounded-md px-4 py-2 mr-4">
-          Sign In
-        </button>
-        <button className="bg-primary text-white rounded-md px-4 py-2">
-          Sign Up
-        </button>
+
+      <div className="flex items-center gap-4 mr-4">
+        <div className="hover:bg-slate-200 rounded-md p-1">
+          <User className="w-6 h-6 text-slate-700" />
+        </div>
+        <div className="hover:bg-slate-200 rounded-md p-1">
+          <ShoppingCart className="w-6 h-6 text-slate-700" />
+        </div>
       </div>
     </header>
   );
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,6 @@
 import type { Route } from "./+types/home";
+import { Header } from "../components/header"; // Adjust the path as needed
+import { Footer } from "../components/footer"; // Adjust the path as needed
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,11 +12,50 @@ export function meta({}: Route.MetaArgs) {
     },
   ];
 }
+type Product = {
+  id: string;
+  name: string;
+  slug: string;
+  series: string | null;
+  description: string | null;
+  price: number;
+  imageUrl: string;
+  categoryId: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
 
-export default function Home() {
+type Products = Product[];
+
+export async function clientLoader({ params }: Route.ClientLoaderArgs) {
+  const response = await fetch(`http://localhost:3000/products`);
+  const products: Products = await response.json();
+  return products;
+}
+
+// HydrateFallback is rendered while the client loader is running
+export function HydrateFallback() {
+  return <div>Loading...</div>;
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  const products = loaderData;
+  console.log("Products:", products);
   return (
     <div>
-      <h1>Welcome to Nepak Point</h1>
+      <Header />
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.imageUrl} alt={product.name} />
+            <h2>{product.name}</h2>
+            <p>Category: {product.categoryId}</p>
+            <p>Series: {product.series}</p>
+            <p>Price: {product.price}</p>
+          </li>
+        ))}
+      </ul>
+      <Footer />
     </div>
   );
 }
