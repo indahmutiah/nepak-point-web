2fb85c6e982a944a9e3f6726d09fe762c106c574
Indah Mutiah Utami MZ <indahmutiahmz@gmail.com>
2025-04-10 16:03:08 +0800

    chore: tidy-up ui

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,14 @@
 import type { Route } from "./+types/home";
-import { Header } from "../components/header"; // Adjust the path as needed
-import { Footer } from "../components/footer"; // Adjust the path as needed
+import { Header } from "~/components/header";
+import { Footer } from "~/components/footer";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -44,17 +52,40 @@ export default function Home({ loaderData }: Route.ComponentProps) {
   return (
     <div>
       <Header />
-      <ul>
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
         {products.map((product) => (
-          <li key={product.id}>
-            <img src={product.imageUrl} alt={product.name} />
-            <h2>{product.name}</h2>
-            <p>Category: {product.categoryId}</p>
-            <p>Series: {product.series}</p>
-            <p>Price: {product.price}</p>
-          </li>
+          <Card
+            key={product.id}
+            className="w-full h-full bg-card rounded-md shadow-md"
+          >
+            <div className="relative w-full h-full">
+              <img
+                src={product.imageUrl}
+                alt={product.name}
+                className="w-full h-100 object-cover rounded-t-md"
+              />
+            </div>
+
+            <CardContent className="p-4">
+              <CardTitle className="text-lg font-semibold mb-2">
+                {product.name}
+              </CardTitle>
+              <div className="flex justify-between text-sm text-muted-foreground">
+                <p>{product.series}</p>
+              </div>
+              <div className="pt-4">
+                <h2 className="text-lg font-black text-primary">
+                  {new Intl.NumberFormat("id-ID", {
+                    style: "currency",
+                    currency: "IDR",
+                    minimumFractionDigits: 0,
+                  }).format(product.price)}
+                </h2>
+              </div>
+            </CardContent>
+          </Card>
         ))}
-      </ul>
+      </div>
       <Footer />
     </div>
   );
