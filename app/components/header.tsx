8370eaddc8f11954feef599220a23effb17ae6fc
Indah Mutiah Utami MZ <indahmutiahmz@gmail.com>
2025-04-11 16:17:57 +0800

    fix: refine searh function

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,7 +1,7 @@
 import { User } from "lucide-react";
 import { ShoppingCart } from "lucide-react";
 import { Input } from "~/components/ui/input";
-import { useState } from "react";
+import { Form } from "react-router";
 
 type Product = {
   id: string;
@@ -16,27 +16,6 @@ type Product = {
   updatedAt: Date;
 };
 export function Header() {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [searchResults, setSearchResults] = useState<Product[]>([]);
-
-  const handleSearch = async (query: string) => {
-    setSearchQuery(query);
-
-    if (query.trim() === "") {
-      setSearchResults([]);
-      return;
-    }
-
-    try {
-      const response = await fetch(
-        `${process.env.BACKEND_API_URL}/products/search?q=${query}`
-      );
-      const data = await response.json();
-      setSearchResults(data);
-    } catch (error) {
-      console.error("Error fetching search results:", error);
-    }
-  };
   return (
     <header className="flex flex-row justify-between items-center w-full h-16 bg-card">
       <a href="/" className="flex flex-row items-center">
@@ -55,27 +34,14 @@ export function Header() {
           <ShoppingCart className="w-6 h-6 text-slate-700" />
         </div>
         <div className="w-80">
-          <Input
-            type="text"
-            placeholder="Search..."
-            className="bg-slate-200 text-slate-400"
-            value={searchQuery}
-            onChange={(e) => handleSearch(e.target.value)}
-          />
-          {searchResults.length > 0 && (
-            <div className="absolute top-full left-0 w-full bg-white shadow-md rounded-md mt-2 z-10">
-              <ul>
-                {searchResults.map((product) => (
-                  <li
-                    key={product.id}
-                    className="p-2 hover:bg-gray-100 cursor-pointer"
-                  >
-                    {product.name}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
+          <Form action="/search">
+            <Input
+              name="q"
+              type="search"
+              placeholder="Search..."
+              className="bg-slate-200 text-slate-400"
+            />
+          </Form>
         </div>
       </div>
     </header>
