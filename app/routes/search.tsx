1a05c6cf4b202b93622a94798caeb430273bdfed
Indah Mutiah Utami MZ <indahmutiahmz@gmail.com>
2025-04-12 10:08:38 +0800

    fix: refine search data

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -1,6 +1,4 @@
 import type { Route } from "./+types/home";
-import { Header } from "~/components/header";
-import { Footer } from "~/components/footer";
 import type { Products } from "~/modules/product/type";
 import { ProductCard } from "~/components/product-card";
 export function meta({}: Route.MetaArgs) {
@@ -26,13 +24,11 @@ export default function Search({ loaderData }: Route.ComponentProps) {
   const products = loaderData;
   return (
     <div>
-      <Header />
       <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
-      <Footer />
     </div>
   );
 }
